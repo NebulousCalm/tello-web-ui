3a8df3f385b5f8b514a49b2be45446a2378bc755
ZachJRichman <zjrichman@outlook.com>
2024-01-19 12:25:23 -0500

    actually working RUNME code

--- static/forms.js
+++ static/forms.js
@@ -10,20 +10,62 @@ const addInput = () =>{
     select.setAttribute('name', 'select');
     select.setAttribute('class', 'selectClass')
 
-    let option = document.createElement("option");
-    option.text = "Forward";
-    option.value = "Forward";
+    // shitty append stuff
 
-    let option2 = document.createElement("option");
-    option2.text = "Backward"
-    option2.value = "Backward"
+    let forward = document.createElement("option");
+    forward.text = "Forward";
+    forward.value = "forward";
+
+    let backward = document.createElement("option");
+    backward.text = "Backward"
+    backward.value = "backward"
+
+    let left = document.createElement("option");
+    left.text = "Left"
+    left.value = "left"
+
+    let right = document.createElement("option");
+    right.text = "Right"
+    right.value = "right"
+
+    let up = document.createElement("option");
+    up.text = "Up"
+    up.value = "up"
+
+    let down = document.createElement("option");
+    down.text = "Down"
+    down.value = "down"
+
+    let anticlockwise = document.createElement("option");
+    anticlockwise.text = "Anti-clockwise"
+    anticlockwise.value = "anticlockwise"
+
+    let clockwise = document.createElement("option");
+    clockwise.text = "Clockwise";
+    clockwise.value = "clockwise";
+
+    let flipforward = document.createElement("option");
+    flipforward.text = "Flip Forward";
+    flipforward.value = "flipforward";
+
+    let flipbackward = document.createElement("option");
+    flipbackward.text = "Flip Backward"
+    flipbackward.value = "flipbackward"
+
+    let flipleft = document.createElement("option");
+    flipleft.text = "Flip Left"
+    flipleft.value = "flipleft"
+
+    let flipright = document.createElement("option");
+    flipright.text = "Flip Right"
+    flipright.value = "flipright"
 
     let parentForm = document.getElementById('parentForm');
     parentForm.append(input, parentForm.lastChild);
     let appendSection = document.getElementsByClassName('planInput');
     parentForm.append(select, parentForm.lastChild);
 
-    select.append(option, option2);
+    select.append(forward, backward, left, right, up, down, flipbackward, flipforward, flipright, flipleft, anticlockwise, clockwise);
 }
 
 document.getElementById('add-input').addEventListener('click', addInput)
